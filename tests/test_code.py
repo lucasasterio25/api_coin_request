171d7414d1cdae293cdf39f069c2968631f5fb42
Lucas Sousa <lucasasterio@hotmail.com>
2026-06-16 10:32:38 -0300

    fix test def name

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,7 +1,7 @@
 from app.services.downloader import *
 from datetime import date
 
-def date_test_coin():
+def test_date_coin():
     date = '2026-01-01'
     downloader = CoinExtraction()
     result = downloader.download_day('bitcoin', date, store_db=False)
